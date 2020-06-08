ccc938729f5333628ff468df2ddcfb53ceceec87
seriousheartgoat <1072382597@qq.com>
2020-06-08 14:48:20 +0800

    gdj init

--- src/main/java/com/woniu/gdj/GdjApplication.java
+++ src/main/java/com/woniu/gdj/GdjApplication.java
@@ -1,9 +1,11 @@
 package com.woniu.gdj;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.woniu.gdj.mapper")
 public class GdjApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/woniu/gdj/service/testService.java
@@ -0,0 +1,4 @@
+package com.woniu.gdj.service;
+
+public class testService {
+}
